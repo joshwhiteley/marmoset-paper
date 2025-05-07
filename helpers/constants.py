3dd8b7dacf6984fccda94420692004c2ca7ab155
joshwhiteley <joshwhiteley89@gmail.com>
2025-05-07 14:18:17 -0400

    Update constants to include delimiters for identification of feature types, as well as basic colors for the feature types.

--- helpers/constants.py
+++ helpers/constants.py
@@ -3,3 +3,16 @@ SEVERE_CLUSTERS = [2, 15, 11, 12, 4, 14, 0, 5]
 
 LESS_SEVERE_COLOR_TUPLE = (127/255, 212/255, 134/255)
 SEVERE_COLOR_TUPLE = (201/255, 174/255, 192/255)
+
+LIDS_DELIMITER = ['AcidicN', 'NeutralN', 'AcidicH', 'NeutralH']
+META_DELIMITER = ['Drug', 'Compound', 'Compound', 'MarmID', 'Lesion', 'Consolidate', 'Contour']
+
+FEATURE_CATEGORY_COLORS = {
+    "marmoset": "tab:red",
+    "simple PK": "tab:orange",
+    "LIDS": "tab:green",
+    "simple equipotent": "tab:blue",
+    "metadata": "tab:grey",
+    "unknown": "tab:black"
+}
+
